230bb18b89d1a582d42a62ebdc3abc2e4b097d4d
aboumaza <ad.boumaza@yahoo.fr>
2020-09-03 23:18:31 +0200

    initial commit

---
+++ src/main/java/dz/sikkens/scanlib/ScanlibApplication.java
@@ -0,0 +1,13 @@
+package dz.sikkens.scanlib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScanlibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScanlibApplication.class, args);
+	}
+
+}
